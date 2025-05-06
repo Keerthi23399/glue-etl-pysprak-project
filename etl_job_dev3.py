6984f01a479b3fcef9cd21265ed09c8597ac846f
Kathleen Rose <kathleen@yliway.com>
2025-05-06 20:00:40 +0530

    update script

--- etl_job_dev3.py
+++ etl_job_dev3.py
@@ -14,7 +14,7 @@ job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
 # Define the S3 paths for input and output
-input_path = "s3://Input-folder-pyspark/sample_data.csv"
+input_path = "s3://input-folder-pyspark/sample-data.csv"
 output_path = "s3://target-glue-job-etl/"
 
 # Read from S3 dev input
@@ -23,12 +23,11 @@ df = spark.read.csv(input_path, header=True, inferSchema=True)
 # Transform - make a column uppercase for example
 df = df.withColumn("name_upper", upper(df["name"]))
 
-# Read CSV
-df = spark.read.csv(input_path, header=True, inferSchema=True)
-
 
 # Write to S3 dev output
-df.write.mode("overwrite").parquet("s3://your-dev-bucket/output/")
+#df.write.mode("overwrite").parquet("s3://target-glue-job-etl/output/")
+df.write.mode("overwrite").option("header", True).csv("s3://target-glue-job-etl/output/")
+
 
 job.commit()
 
